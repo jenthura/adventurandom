b1613cf6c9f423ad5390e59ed096d04d5cab8780
jenthura <joshuasth@gmail.com>
2020-08-06 17:48:44 -0500

    changed api_endpoint to the right url

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import back from '../../img/back_button.png';
+
 
 export default class About extends Component {
 
@@ -15,13 +17,16 @@ export default class About extends Component {
             <div className="main">
                 <div className="container centered">
                     <h1>About</h1>
-                    <p>Hi! I'm Chatchawan L Suwaratana, an aspiring fullstack webdeveloper! I'd love for you to check out my other projects here!</p>
-                    LINK TO GITHUB
-                    <p>And if you'd like, you can follow my progress on social media!</p>
-                    SOCIAL MEDIA LINKS
+                    <p>Hi! I'm Chatchawan L Suwaratana, and I made this app as a fullstack practice project for the Thinkful bootcamp. I'll update it over time with new stuff!</p>
+                    <h2>Thanks to</h2>
+                    <p><b>Sunitha Kandalam</b>, my TA, for her daily check-ins to keep me on track!</p>
+                    <p><b>Nash Worth</b>, my mentor, for his pep talks and advice!</p>
+                    <a href="https://github.com/jenthura/adventurandom">Github repository</a>
+                    <p>If you'd like, you can follow my progress!</p>
+                    <a href="https://twitter.com/home">Twitter</a>
                 </div>
                 <div className="container centered">
-                    <Link to='/'><button>Back</button></Link>
+                    <Link to='/'><img src={back} alt='back_button' /></Link>
                 </div>
             </div>
         )
--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-    API_ENDPOINT: 'postgres://rwmbggjlfgbxdf:d698d781a1331989ee0cfb4b7c2d2cd5235ae04465370c5fb29099ea8e63db27@ec2-107-22-7-9.compute-1.amazonaws.com:5432/d3bve103md6fgq',
+  API_ENDPOINT: (process.env.REACT_APP_ENV === 'production')
+  ? 'https://calm-mesa-89954.herokuapp.com/'
+  : 'http://localhost:8000',
   }
