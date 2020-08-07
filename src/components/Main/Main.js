6c276ffbe6ca715753fa0e1d697424fd36d6025a
jenthura <joshuasth@gmail.com>
2020-08-06 21:06:34 -0500

    changed loca to location in text

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -21,9 +21,9 @@ export default class Main extends Component {
         </div>
         <div className='container tutorial'>
           <h2 className='centered'>Instructions</h2>
-          <h3>Loca</h3>
+          <h3>Location</h3>
           <p>
-            Currently the loca does not affect anything. In future versions,
+            Currently the location does not affect anything. In future versions,
             this will change the difficulty of the adventure, and change which
             monsters you can meet. e.g. Vampires in the Castle, Zombies in the
             Graveyard, etc...
@@ -31,7 +31,7 @@ export default class Main extends Component {
           <h3>Number of Encounters</h3>
           <p>
             This is how many times you will meet a monster and fight. The more
-            of these you add, the more experience points you can stand to gain,
+            of these you add, the more gold you can stand to gain,
             but the difficulty also rises.
           </p>
           <h3>Character Name</h3>
