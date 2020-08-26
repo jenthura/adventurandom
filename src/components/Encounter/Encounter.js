7efec682001b5404eb75c3cb60392d5f55c9ab4f
jenthura <joshuasth@gmail.com>
2020-08-26 11:18:21 -0500

    favicon

--- src/components/Encounter/Encounter.js
+++ src/components/Encounter/Encounter.js
@@ -8,57 +8,57 @@ export default class Encounter extends Component {
   static contextType = StoryContext;
 
   componentDidMount() {
-    MonsterApiService.getMonster()
-      .then((monster) => this.context.setMonster(monster))
-      .catch((error) => console.log('GET monster error', error));
+    // MonsterApiService.getMonster()
+    //   .then((monster) => this.context.setMonster(monster))
+    //   .catch((error) => console.log('GET monster error', error));
 
-    CharApiService.getCharById(this.context.charClass)
-      .then((char) => this.context.setChar(char))
-      .catch((error) => console.log('GET char error', error));
-
-    CharApiService.getCharAttacks(this.context.charClass)
-      .then((charAttacks) => this.context.setCharAttacks(charAttacks))
-      .then(this.context.toggleIsLoading())
-      .catch((error) => console.log('GET char attacks error', error))
+    // CharApiService.getCharAttacks(this.context.charClass)
+    //   .then((charAttacks) => this.context.setCharAttacks(charAttacks))
+    //   .then(this.context.toggleIsLoading())
+    //   .catch((error) => console.log('GET char attacks error', error))
       
   }
 
   render() {
-    const { char, monster, charAttacks, isLoading} = this.context;
+    const { isLoading} = this.context;
+    const { monsterName, monsterHitpoints, attackName, attackDamageDie, attackDamageType } = this.props
     if (isLoading === true) {
       return 'Loading...'
     }
-    if (monster[0].monster_name==='' || charAttacks[0].attack_name==='') {
-      return 'Loading...';
-    }
+    // if (monster[0].monster_name==='' || charAttacks[0].attack_name==='') {
+    //   return 'Loading...';
+    // }
 
-    const monsterId = Math.floor(Math.random() * monster.length);
-    const attackId = Math.floor(Math.random() * charAttacks.length);
-    const charDamage = Math.floor(Math.random() * 7);
-    const monsterDamage = Math.floor(Math.random() * monster[monsterId].damage_die)
-    
+    const charDamage = Math.floor(Math.random() * attackDamageDie);
+    const monsterDamage = Math.floor(Math.random() * 12)
+
+    // console.log(charAttacks)
+    // console.log(charAttacks[attackId])
+    // console.log(charAttacks[attackId].damage_die)
+
     return (
       <div className='encounter'>
-        A {monster[monsterId].monster_name} pops out!
+        A {monsterName} pops out!
         <br />
         <br />
-        You attack a {monster[monsterId].monster_name} with your{' '}
-        {charAttacks[attackId].attack_name} for{' '}
-        {charDamage} damage!{' '}
+        You attack a {monsterName} with your{' '}
+        {attackName} for{' '}
+        {charDamage} {attackDamageType} damage!{' '}
         {`It has ${
-          monster[monsterId].hitpoints - charDamage
+          monsterHitpoints - charDamage
         } hitpoints remaining`}
         <br />
         <br />
-        The {monster[monsterId].monster_name} attacks you for{' '}
+        The {monsterName} attacks you for{' '}
         {monsterDamage} damage!
         <br />
         <br />
-        You attack a {monster[monsterId].monster_name} with your{' '}
-        {charAttacks[attackId].attack_name} for{' '}
-        {charDamage + 50} damage!{' '}
-        The {monster[monsterId].monster_name} dies horribly!
+        You attack a {monsterName} with your{' '}
+        {attackName} for{' '}
+        {charDamage + 50} {attackDamageType} damage!{' '}
+        The {monsterName} dies horribly!
         <br />
+        <hr />
         <br />
       </div>
     );
--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import CharApiService from '../../services/char-api-service';
+import MonsterApiService from '../../services/monster-api-service';
 import LocaFlavorApiService from '../../services/location-api-service';
-import StoryContext from '../../contexts/StoryContext'
-import Encounter from '../Encounter/Encounter'
+import StoryContext from '../../contexts/StoryContext';
+import Encounter from '../Encounter/Encounter';
 import { Link } from 'react-router-dom';
 import restart from '../../img/restart_button.png';
 import treasure from '../../img/treasure.jpg';
@@ -16,30 +17,37 @@ export default class Story extends Component {
         const { charClass } = this.context;
         CharApiService.getCharById(charClass)
           .then(char => this.context.setChar(char))
-          .catch(this.context.setError)
+          .catch(this.context.setError);
 
+        MonsterApiService.getMonster()
+          .then((monster) => this.context.setMonster(monster))
+          .catch((error) => console.log('GET monster error', error));
 
         LocaFlavorApiService.getLocaFlavor()
           .then(locaFlavor => this.context.setLocaFlavor(locaFlavor[Math.floor(Math.random()*(locaFlavor.length))]))
-          .catch(this.context.setError)
+          .catch(this.context.setError);
+
+        CharApiService.getCharAttacks(this.context.charClass)
+          .then((charAttacks) => this.context.setCharAttacks(charAttacks))
+          .then(this.context.toggleIsLoading())
+          .catch((error) => console.log('GET char attacks error', error))
     }
 
     render() {
-        const {locaFlavor, charName, dungeonLoca, char, numberOfEncounters} = this.context
+        const {locaFlavor, charName, dungeonLoca, char, charAttacks, numberOfEncounters, monster} = this.context
         if (charName === '') {
-            return (
-                <div className='centered container'>
-                    Sorry, no data found.<br/><br/>
-                    <div className='centered'>
-                        <Link to='/story-form'><img src={restart} alt='start_button'/></Link>
-                    </div>
-                </div>
-            )
-        }
-        const emptyArray = []
+            this.props.history.push('/story-form');
+        };
+        const randomMonsters = []
         for (let i = 0; i < numberOfEncounters; i ++) {
-            emptyArray.push('count')
-        }
+            let randomMonsterId = Math.floor(Math.random()*monster.length)
+            randomMonsters.push(monster[randomMonsterId])
+        };
+        const randomAttacks = []
+        for (let i = 0; i < numberOfEncounters; i ++) {
+            let randomAttackId = Math.floor(Math.random()*charAttacks.length)
+            randomAttacks.push(charAttacks[randomAttackId])
+        };
 
         return (
             <div className='container'>
@@ -47,7 +55,16 @@ export default class Story extends Component {
                     <b>You are {charName} the {char.class_name}. You venture into the "<i>{dungeonLoca} {locaFlavor.location_flavor_string}</i>" to seek fortune and excitement! You enter the danger zone! And...</b>
                     <br />
                     <br />
-                    {emptyArray.map(element => <Encounter/>)}
+
+                    {randomMonsters.map(monster => <Encounter 
+                        monsterHitpoints={monster.hitpoints} 
+                        monsterLength={monster.length} 
+                        monsterName={monster.monster_name}
+                        attackName={randomAttacks.map(attack => attack.attack_name)[0]}
+                        attackDamageDie={randomAttacks.map(attack => attack.damage_die)[0]}
+                        attackDamageType={randomAttacks.map(attack => attack.damage_type)[0]}
+                    />)}
+
                     You reach the end of the {dungeonLoca}! You find {numberOfEncounters*10} gold coins!<br /><br />
                     <img src={treasure} alt='A chest full of gold coins and loot!'/>
                     <div className='centered'>
@@ -57,7 +74,7 @@ export default class Story extends Component {
             </div>
         )
     }
-}
+};
 
 
 // {locaFlavor[locaFlavorNum]['id']}
